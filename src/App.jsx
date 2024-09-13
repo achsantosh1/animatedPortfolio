a506df641db9c05deefc9ea7573b9b63e63d4ffd
achsantosh1 <santosh9860476238@gmail.com>
2024-09-13 22:57:06 +0545

    fix :import update for portfolio

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Cursor from "./components/cursor/Cursor";
 import Hero from "./components/hero/Hero";
 import Navbar from "./components/navbar/Navbar";
 import Parallax from "./components/parallax/Parallax";
-import Portfolio from "./components/portfolio/portfolio";
+import Portfolio from "./components/portfolio/Portfolio";
 import Services from "./components/services/Services";
 
 function App() {
@@ -24,7 +24,7 @@ function App() {
       <section id="Portfolio">
         <Parallax type="portfolio" />
       </section>
-    <Portfolio/>
+         <Portfolio/>
       <section id="Contact"><Contact/></section>
 
     </div>
